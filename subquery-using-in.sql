1c7e508951ce2631c49c03c627a7e69dbb54e0ce
= <sasukegc@gmail.com>
2021-03-17 21:27:10 -0300

    add file subquery-using-in.sql

---
+++ subquery-using-in.sql
@@ -0,0 +1,14 @@
+SELECT
+	*
+FROM
+	customers c
+WHERE
+	c.customerid IN (
+	SELECT
+		e.customerid
+	FROM
+		engagements e
+	INNER JOIN entertainers et ON
+		et.entertainerid = e.entertainerid
+	INNER JOIN entertainer_styles es ON
+		es.entertainerid = e.entertainerid )
